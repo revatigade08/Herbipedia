7c46dd0b2bfd5e30f4f566721f06ffdf1a42644c
Tanvi Gunwant Pohankar <pohankartanvi497@gmail.com>
2026-02-14 16:17:40 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -2,5 +2,10 @@ import { auth } from "./firebase-config.js";
 import { createUserWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-
 console.log("Register JS connected");
+
+if (auth) {
+    console.log("Auth loaded successfully");
+} else {
+    console.log("Auth NOT loaded");
+}
