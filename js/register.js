79dbf353ad1131b3ae66f4098520d53437dd2d4c
Tanvi Gunwant Pohankar <pohankartanvi497@gmail.com>
2026-02-14 16:10:35 +0530

    Import Firebase authentication functions in register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,6 @@
+import { auth } from "./firebase-config.js";
+import { createUserWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+
 console.log("Register JS connected");
