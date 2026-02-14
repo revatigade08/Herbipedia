315b868e32d16740210a36bf05e8de1085f5f470
Tanvi Gunwant Pohankar <pohankartanvi497@gmail.com>
2026-02-14 16:25:24 +0530

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,38 @@
+import { auth } from "./firebase-config.js";
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+
+// Password toggle
+const passwordInput = document.getElementById("password");
+const toggleBtn = document.getElementById("togglePassword");
+
+toggleBtn.addEventListener("click", () => {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    toggleBtn.textContent = "Hide";
+  } else {
+    passwordInput.type = "password";
+    toggleBtn.textContent = "Show";
+  }
+});
+
+// Login logic
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+
+    alert("Login successful!");
+    window.location.href = "index.html";
+
+  } catch (error) {
+    alert("Login failed!");
+    window.location.href = "index.html";
+  }
+});
