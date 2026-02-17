9e5e8230a0fe9c724210596a9e7497989e1dd19d
Tanvi Gunwant Pohankar <pohankartanvi497@gmail.com>
2026-02-17 14:55:31 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,10 +2,14 @@ import { auth } from "./firebase-config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
+    console.log("Auth state triggered");
+
     if (user) {
-        console.log("Logged in user:", user.email);
+        console.log("User UID:", user.uid);
+        console.log("User Email:", user.email);
+        document.body.innerHTML += `<h2>Welcome ${user.email}</h2>`;
     } else {
-        console.log("No user logged in");
+        console.log("No user found");
         window.location.href = "login.html";
     }
 });
